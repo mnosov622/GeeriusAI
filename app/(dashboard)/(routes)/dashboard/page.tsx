0d1575c4de2fd9b78dfb4f5e62aeacbf8fc05438
Maxim <mnosov622@gmail.com>
2024-07-17 10:29:03 -0400

    add authentication routes

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from '@clerk/nextjs';
+
 export default function Dashboard() {
 	return (
 		<div>
 			<h1>Dashboard</h1>
+			<UserButton afterSignOutUrl="/" />
 		</div>
 	);
 }
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,8 +1,19 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
 export default function Landing() {
 	return (
 		<div>
 			<h1>GenerAI</h1>
 			<p>AI platform</p>
+			<div>
+				<Link href="/sign-in">
+					<Button>Login</Button>
+				</Link>
+				<Link href="/sign-up">
+					<Button>Register</Button>
+				</Link>
+			</div>
 		</div>
 	);
 }
