9414fdf953a3b7371529f08848d9951b650995f0
Maxim <mnosov622@gmail.com>
2024-07-18 20:05:11 -0400

    init conversations page

--- constants/index.ts
+++ constants/index.ts
@@ -7,6 +7,7 @@ import {
 	Settings,
 	VideoIcon,
 } from 'lucide-react';
+import * as z from 'zod';
 
 export const TOOLS = [
 	{
@@ -89,3 +90,9 @@ export const ROUTES = [
 		href: '/settings',
 	},
 ];
+
+export const formSchema = z.object({
+	prompt: z.string().min(1, {
+		message: 'Prompt is required',
+	}),
+});
