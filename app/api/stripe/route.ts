762c064ae6b3224ca1751af6fa5741bdd849af03
Maxim <mnosov622@gmail.com>
2024-07-23 09:47:44 -0400

    initialize stripe payments

---
+++ app/api/stripe/route.ts
@@ -0,0 +1,67 @@
+import prismadb from '@/lib/prisma.db';
+import { stripe } from '@/lib/stripe';
+import { absoluteUrl } from '@/lib/utils';
+import { auth, currentUser } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
+
+const settingsUrl = absoluteUrl('/settings');
+
+export async function GET() {
+	try {
+		const { userId } = auth();
+
+		const user = await currentUser();
+
+		if (!userId || !user) {
+			return new NextResponse('Unauthorized', { status: 401 });
+		}
+
+		const userSubscription = await prismadb.userSubscription.findUnique({
+			where: {
+				userId,
+			},
+		});
+
+		if (userSubscription && userSubscription.stripeCustomerId) {
+			const stripeSession = await stripe.billingPortal.sessions.create({
+				customer: userSubscription.stripeCustomerId,
+				return_url: settingsUrl,
+			});
+
+			return new NextResponse(JSON.stringify({ url: stripeSession.url }));
+		}
+
+		const stripeSession = await stripe.checkout.sessions.create({
+			success_url: settingsUrl,
+			cancel_url: settingsUrl,
+			payment_method_types: ['card'],
+			mode: 'subscription',
+			billing_address_collection: 'auto',
+			customer_email: user.emailAddresses[0].emailAddress,
+			line_items: [
+				{
+					price_data: {
+						currency: 'usd',
+						product_data: {
+							name: 'GenerAI Pro',
+							description: 'Unlimited AI Generations',
+						},
+						unit_amount: 1000,
+						recurring: { interval: 'month' },
+					},
+					quantity: 1,
+				},
+			],
+
+			metadata: {
+				userId,
+			},
+		});
+
+		return new NextResponse(JSON.stringify({ url: stripeSession.url }));
+	} catch (e) {
+		console.log('Stripe error', e);
+
+		return new NextResponse('Internal error', { status: 500 });
+	}
+}
