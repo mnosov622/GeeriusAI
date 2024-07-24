aa33d8dc9412dac4c595cda5c94a6e4d6f4d7d8d
Maxim <mnosov622@gmail.com>
2024-07-24 11:26:01 -0400

    update testimonials

--- components/landing-content.tsx
+++ components/landing-content.tsx
@@ -1,32 +1,38 @@
 'use client';
 
+import Image from 'next/image';
+import testimonal1 from '../public/testimonials/testimonial-1.jpg';
+import testimonal2 from '../public/testimonials/testimonial-2.jpg';
+import testimonal3 from '../public/testimonials/testimonial-3.jpg';
+import testimonal4 from '../public/testimonials/testimonial-4.jpg';
+
 import { Card, CardContent, CardHeader, CardTitle } from './ui/card';
 
 const testimonials = [
 	{
 		name: 'Antonio',
-		avatar: 'https://randomuser.me/api/portraits',
+		avatar: testimonal1,
 		title: 'Software Engineer',
 		description:
-			'This is the best app if you want to create some content for your needs. I love it!',
+			'This is the best app if you want to create some copyright-free content for your application. I love it!',
 	},
 	{
-		name: 'Julia',
-		avatar: 'https://randomuser.me/api/portraits/women/44.jpg',
+		name: 'Julien',
+		avatar: testimonal2,
 		title: 'Graphic Designer',
 		description:
 			'Incredible tool! It has streamlined my design process, and the results are always top-notch.',
 	},
 	{
-		name: 'Marcus',
-		avatar: 'https://randomuser.me/api/portraits/men/32.jpg',
+		name: 'Sophia',
+		avatar: testimonal3,
 		title: 'Digital Marketer',
 		description:
 			'Using this app has significantly boosted our online engagement. Highly recommend it for marketing professionals.',
 	},
 	{
-		name: 'Sophia',
-		avatar: 'https://randomuser.me/api/portraits/women/50.jpg',
+		name: 'Marcus',
+		avatar: testimonal4,
 		title: 'Content Creator',
 		description:
 			'As a content creator, this app has been a game-changer for me. It’s easy to use and very effective.',
@@ -45,13 +51,23 @@ export default function LandingContent() {
 					>
 						<CardHeader>
 							<CardTitle className="flex items-center gap-x-2">
-								<div>
-									<p className="text-lg">{testimonial.name}</p>
-									<p className="text-zinc-400 text-sm">{testimonial.title}</p>
+								<div className="flex items-center gap-x-2">
+									<Image
+										src={testimonial.avatar}
+										alt={testimonial.name}
+										width={50}
+										height={50}
+										className="rounded-full"
+									/>
+
+									<div className="flex flex-col">
+										<p className="text-lg">{testimonial.name}</p>
+										<p className="text-zinc-400 text-sm">{testimonial.title}</p>
+									</div>
 								</div>
 							</CardTitle>
 							<CardContent className="pt-4 px-0">
-								<p className="text-sm">{testimonial.description}</p>
+								<p>{testimonial.description}</p>
 							</CardContent>
 						</CardHeader>
 					</Card>
